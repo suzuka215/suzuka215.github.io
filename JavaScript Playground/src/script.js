65602d44ba1229b78869d848eb9964af52690bdb
suzuka215 <109153495+suzuka215@users.noreply.github.com>
2023-07-06 23:43:58 +0900

    Add files via upload

---
+++ JavaScript Playground/src/script.js
@@ -0,0 +1,25 @@
+//受験番号：2023037
+
+//課題1
+message="Hello World"
+console.log(message);
+
+//課題2
+let array = [1, 2, 3, 4, 5];
+console.log(array);
+
+//課題3
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
+//課題4
+function loopsum() {
+  let sum = 0;
+  for (let i = 1; i <= 100; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(loopsum());
